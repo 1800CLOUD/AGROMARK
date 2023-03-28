ae38eccf96ce3a2d3cd949081a2ba3ab249490ab
dfcastano <diegoferledesma@gmail.com>
2023-03-28 16:08:04 -0500

    [IMP] Report margen product

--- report_margen_product/reports/report_margen.py
+++ report_margen_product/reports/report_margen.py
@@ -23,62 +23,6 @@ class ReportInvoice(models.TransientModel):
     xls_filename = fields.Char()
     
 
-    def compute_report(self):
-        def _add_where(table, fld, vl):
-            return f" AND {table}.{fld} IN ({','.join(str(x.id) for x in vl)})"
-        
-        cr = self._cr
-        wh = ''
-        uid = self.env.user.id
-        dt_from = str(self.date_from)
-        dt_to = str(self.date_to)
-        dt_now = fields.Datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if self.product_ids:
-            wh += _add_where('sol', 'product_id', self.product_ids)
-
-        if self.brand_ids:
-            wh += _add_where('pt', 'product_brand_id', self.brand_ids)
-
-            
-        #add_fields_insert, add_fields_select, add_fields_from = self.extended_compute_fields()
-            
-        cr.execute(f'DELETE FROM report_margen_product_line WHERE create_uid = {uid}')
-            
-        qry = f'''
-            INSERT INTO report_margen_product_line (product_id, product_brand_id, quantity, income, cost,
-                utility, cost_effectiveness, margin_utility, create_date, write_date)
-                    SELECT
-                            sol.product_id, 
-                            pt.product_brand_id,
-                            SUM(sol.product_uom_qty), 
-                            SUM(sol.price_subtotal),
-                            svl.unit_cost*(SUM(sol.product_uom_qty)),
-                            SUM(sol.price_subtotal) - svl.unit_cost*(SUM(sol.product_uom_qty)),
-                            (SUM(sol.price_subtotal) - svl.unit_cost*SUM(sol.product_uom_qty)) / SUM(sol.price_subtotal),
-                            (SUM(sol.price_subtotal) - svl.unit_cost*SUM(sol.product_uom_qty)) / (svl.unit_cost*(SUM(sol.product_uom_qty))),
-                            '{dt_now}', 
-                            '{dt_now}'
-
-                        FROM sale_order_line sol
-                            INNER JOIN sale_order so ON sol.order_id = so.id 
-                            INNER JOIN product_product pp ON sol.product_id = pp.id 
-                            INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
-                            INNER JOIN stock_move sm ON sol.id = sm.sale_line_id 
-                            LEFT JOIN stock_valuation_layer svl ON sm.id = svl.stock_move_id
-                            LEFT JOIN product_brand pb ON pb.id = pt.product_brand_id
-
-                        WHERE
-                            so.state = 'done' AND
-                            so.date_order BETWEEN   '{dt_from}' AND '{dt_to}' {wh}
-                        GROUP BY
-                        sol.product_id,
-                        pt.product_brand_id,
-                        svl.unit_cost
-                        
-        '''
-        cr.execute(qry)
-    
-
     def _compute_excel(self):
         def _add_wh(fld, tbl):
             return " AND {} IN ({})".format(fld, ','.join(str(x.id) for x in tbl))
@@ -165,44 +109,8 @@ class ReportInvoice(models.TransientModel):
         self.xls_file = base64.encodebytes(xlsx_data)
         self.xls_filename = "report_invoice.xlsx"
     
-    def analysis(self):
-        self.compute_report()
-        dt_from = str(self.date_from)
-        dt_to = str(self.date_to)
-        domain = [('date_order', '>', dt_from), ('date_order', '<=', dt_to)]
-        return_dict = {}
-        if 'pivot' in self._context:
-            view_id = self.env['ir.ui.view'].search([('name','=','view_margen_report_pivot')], limit=1)
-            return_dict.update({'view_mode': 'pivot'})
-        else:
-            view_id = self.env['ir.ui.view'].search([('name','=','view_margen_report_pivot')], limit=1)
-            search_view_id = self.env['ir.ui.view'].search([('name','=','report_margen_product_line_search')], limit=1)
-            return_dict.update({
-                'search_view_id': search_view_id.id,
-                'view_mode': 'pivot'})
-        return_dict.update({
-            'domain': domain,
-            'name': 'Margen de producto',
-            'view_type': 'pivot',
-            'view_id': view_id.id,
-            'res_model': 'report.margen.product.line',
-            'type': 'ir.actions.act_window',
-        })
-        return return_dict
+    
     
 
-class ReportMargenProductLine(models.Model):
-    _name = 'report.margen.product.line'
-    _description = 'Lineas del reporte margen de productos'
-    _order = 'product_id,income,cost,utility'
-    _report_vacuum = True
-    
-    product_id = fields.Many2one('product.product', 'Producto', copy=False, readonly=True, required=True, index=True)
-    product_brand_id = fields.Many2one(comodel_name="product.brand", string="Marca", copy=False, readonly=True, index=True)
-    quantity = fields.Float('Cantidad', copy=False, readonly=True, required=True)
-    income = fields.Float('Ingresos', copy=False, readonly=True, required=True)
-    cost = fields.Float('Costo', copy=False, readonly=True, required=True)
-    utility = fields.Float('Utilidad', copy=False, readonly=True, required=True)
-    cost_effectiveness = fields.Float('rentabilidad', copy=False, readonly=True, required=True)
-    margin_utility = fields.Float('Utilidad%', copy=False, readonly=True, required=True)
+
     
