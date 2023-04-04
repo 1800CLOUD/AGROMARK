d462ac36f442b83249bbbf252ca6ebc443b4cd22
dfcastano <diegoferledesma@gmail.com>
2023-04-04 18:51:04 -0500

    [IMP] Report margen product in Agromark

--- report_margen_product/reports/report_margen.py
+++ report_margen_product/reports/report_margen.py
@@ -16,8 +16,8 @@ class ReportInvoice(models.TransientModel):
     _description = 'Reporte de margen de productos'
     
     name = fields.Char('Nombre', default='Informe de Margen de Producto', readonly=True)
-    date_from = fields.Date('Desde', required=True, default=(fields.Datetime.now() - relativedelta(month=1)))
-    date_to = fields.Date('Hasta', required=True, default=(fields.Datetime.now()).date())
+    date_from = fields.Datetime('Desde', required=True, default=(fields.Datetime.now() - relativedelta(month=1)))
+    date_to = fields.Datetime('Hasta', required=True, default=(fields.Datetime.now()).date())
     product_ids = fields.Many2many('product.product', string='Productos', copy=False) 
     brand_ids = fields.Many2many('product.brand', string='Marcas')
     xls_file = fields.Binary(string="XLS file")
@@ -25,46 +25,49 @@ class ReportInvoice(models.TransientModel):
     
 
     def _compute_excel(self):
-        def _add_wh(fld, tbl):
-            return " AND {} IN ({})".format(fld, ','.join(str(x.id) for x in tbl))
+        def _add_where(table, fld, vl):
+            return f" AND {table}.{fld} IN ({','.join(str(x.id) for x in vl)})"
     
         #APLICAR FILTROS
         wh = '' 
         if self.product_ids:
-            wh += _add_wh('sol.product_id', self.product_ids)
-        if self.product_ids:
-            wh += _add_wh('pt.product_brand_id', self.brand_ids)
+            wh += _add_where('sol', 'product_id', self.product_ids)
+        if self.brand_ids:
+            wh += _add_where('pt', 'product_brand_id', self.brand_ids)
 
         cr = self.env.cr
         dt_from = str(self.date_from)
         dt_to = str(self.date_to)
         cr.execute(f'''SELECT
-                            pt.name, 
+                            DISTINCT pt.name, 
                             pb.name,
-                            SUM(sol.product_uom_qty) qty, 
-                            SUM(sol.price_subtotal) price,
-                            SUM(svl.unit_cost*sol.product_uom_qty) AS cost,
+                            SUM(sol.product_uom_qty),
+                            SUM(sol.price_subtotal),
+                            SUM(svl.unit_cost*sol.product_uom_qty),
                             SUM(sol.price_subtotal) - SUM(svl.unit_cost*sol.product_uom_qty),
-                            (SUM(sol.price_subtotal) - SUM(svl.unit_cost*sol.product_uom_qty)) / SUM(sol.price_subtotal),
-                            (SUM(sol.price_subtotal) - SUM(svl.unit_cost*sol.product_uom_qty)) / (SUM(svl.unit_cost*sol.product_uom_qty))
+                            ((SUM(sol.price_subtotal) - SUM(svl.unit_cost*sol.product_uom_qty)) / SUM(sol.price_subtotal)),
+                            ((SUM(sol.price_subtotal) - SUM(svl.unit_cost*sol.product_uom_qty)) / (SUM(svl.unit_cost*sol.product_uom_qty)))
+                            
 
                         FROM sale_order_line sol
                             INNER JOIN sale_order so ON sol.order_id = so.id 
                             INNER JOIN product_product pp ON sol.product_id = pp.id 
                             INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
+                            INNER JOIN product_brand pb ON pb.id = pt.product_brand_id
                             INNER JOIN stock_move sm ON sol.id = sm.sale_line_id 
                             INNER JOIN stock_valuation_layer svl ON sm.id = svl.stock_move_id
-                            INNER JOIN product_brand pb ON pb.id = pt.product_brand_id
+
 
                         WHERE
                             pt.detailed_type = 'product' AND 
                             sol.product_uom_qty = sol.qty_invoiced AND
                             so.state = 'done' AND
-                            so.date_order BETWEEN   '{dt_from}' AND '{dt_to}' {wh}
-                        GROUP BY
+                            so.date_order BETWEEN   '{dt_from}' AND '{dt_to}' 
+                            {wh}
+                        GROUP BY 
                         pt.name,
-                        pb.name,
-                        svl.unit_cost
+                        pb.name
+                        
                       ''')
         result = cr.fetchall()
         return result
@@ -90,7 +93,7 @@ class ReportInvoice(models.TransientModel):
         titles_format.set_align("center")
         titles_format.set_bold()
         worksheet.set_column("A:H", 22)
-        #worksheet.set_row(0, 25)
+        worksheet.set_row(0, 25)
         
         col_num = 0
         for title in titles:
@@ -110,7 +113,7 @@ class ReportInvoice(models.TransientModel):
         xlsx_data = output.getvalue()
 
         self.xls_file = base64.encodebytes(xlsx_data)
-        self.xls_filename = "report_invoice.xlsx"
+        self.xls_filename = "report_margen.xlsx"
     
     
     
