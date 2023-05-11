bf21e80c42e585ff7b3572264049a2db240e24ec
dfcastano <diegoferledesma@gmail.com>
2023-05-11 13:40:22 -0500

    [REVERT] report balance

--- account_report_agromark/__manifest__.py
+++ account_report_agromark/__manifest__.py
@@ -14,7 +14,7 @@
     'website': "https://1-800cloud.com/",
     'license': 'OPL-1',
     'category': 'Account/Report',
-    'version': '15.0.0.1.9',
+    'version': '15.0.0.2.0',
     'depends': [
         'account_report',
     ],
