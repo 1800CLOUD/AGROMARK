c4c9a5462bf0fcf7652ab9cae79dd9d9ba14e8d9
dfcastano <diegoferledesma@gmail.com>
2023-08-11 16:36:09 -0500

    [IMP] Reporte de compras, se añaden columnas de descuento y referencia de facturas

--- purchase_reports/__manifest__.py
+++ purchase_reports/__manifest__.py
@@ -11,15 +11,15 @@
     'website': 'http://www.1-800cloud.com',
     'category': 'Purchase/Purchase',
     'license': 'LGPL-3',
-    'version': '15.0.0.0.6',
+    'version': '15.0.0.0.7',
     'depends': [
         'base_setup',
         'purchase',
         'account',
     ],
     'data': [
-        # 'security/ir.model.access.csv',
-         'reports/purchase_report.xml',
-         'views/menuitems.xml',
+        'security/ir.model.access.csv',
+        'reports/purchase_report.xml',
+        'views/menuitems.xml',
     ],
 }
